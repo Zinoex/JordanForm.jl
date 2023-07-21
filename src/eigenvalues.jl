56b140492ca14b92effb60ea65542441e8478776
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-21 12:58:18 +0200

    Compute charpoly using Berkowitz algorithm (tested)

--- src/eigenvalues.jl
+++ src/eigenvalues.jl
@@ -1,54 +1,46 @@
 function radical_eigenvalues(A::AbstractMatrix{T}) where {T <: IntOrRational}
-    n = checksquare(A)
-    A = convert.(Rational{eltype(A)}, A)
+    n = LinearAlgebra.checksquare(A)
     
     p = charpoly(A)
 end
 
-function charpoly(A::AbstractMatrix{<:Rational})
-    n = checksquare(A)
-
-    p = berkowitz_vector(A, n)
+function charpoly(A::AbstractMatrix)
+    p = berkowitz_vector(A)
 
     return p
 end
 
 
-function berkowitz_vector(A::AbstractMatrix{T}, n) where {T}
-    M = ones(T, 1)
+function berkowitz_vector(A::AbstractMatrix{T}) where {T}
+    n = LinearAlgebra.checksquare(A)
+    @assert n >= 1
 
-    for i in 1:(n - 1)  
+    M = T[1, -A[end, end]]
+
+    for i in 2:n
         # Loop form the back as that enables storing a single vector T,
         # rather than storing a matrix for each iterations before reducing.
+        
+        # i is the size of the A block to be partitioned into [a R; C Asub]
 
-        if i == 1
-            M = T[1; -A[end, end]] * M
-        elseif i == 2
-            C = A[end, end - 1]
-            R = A[end - 1, end]
-            a = A[end - 1, end - 1]
+        Asub = @view(A[end - i + 2:end, end - i + 2:end])
+        C = @view(A[end - i + 2:end, end - i + 1])      # Note: C is stored sequentially (it is a column)
+        R = @view(A[end - i + 1, end - i + 2:end])      # Note R is not stored sequentially
+        a = A[end - i + 1, end - i + 1]
 
-            M = NonSquareLowerTriangularToeplitz(T[1, -a,  -R * C], i + 1) * M
-        else
-            Asub = @view(A[end - i:end, end - i:end])
-            C = @view(A[end - i:end, end - i - 1])      # Note: C is stored sequentially (it is a column)
-            R = @view(A[end - i - 1, end - i:end])      # Note R is not stored sequentially
-            a = A[end - i - 1, end - i - 1]
+        N = Vector{T}(undef, i + 1)
+        N[1] = 1
+        N[2] = -a
 
-            N = Vector{T}(undef, i + 1)
-            N[1] = 1
-            N[2] = -a
+        K = -transpose(R)
+        N[3] = K * C
 
-            K = -R
-            N[3] = K * C     # Multiplying by C is faster than multiplying by R due to the storage. Hence we multiply by C in the iteration.
-
-            for j in 2:(i - 1)
-                K *= Asub
-                N[j + 2] = K * C
-            end
-
-            M = NonSquareLowerTriangularToeplitz(N) * M
+        for j in 2:(i - 1)
+            K *= Asub
+            N[j + 2] = K * C
         end
+
+        M = NonSquareLowerTriangularToeplitz(N, i) * M
     end
 
     return M
