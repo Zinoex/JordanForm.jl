90780525d1c20611d2569f24b6b5f25cdaea955b
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-07-24 18:49:51 +0200

    WIP compute Jordan form with LinearAlgebraX

--- src/JordanForm.jl
+++ src/JordanForm.jl
@@ -1,6 +1,6 @@
 module JordanForm
 
-using LinearAlgebra
+using LinearAlgebra, BlockDiagonals, RowEchelon, LinearAlgebraX
 using Graphs
 using Symbolics
 
@@ -9,6 +9,86 @@ const IntOrRational = Union{Integer, Rational}
 include("toeplitz.jl")
 include("eigenvalues.jl")
 
+export JordanBlock, JordanCanonicalForm, JordanFactorization
 export jordan_form
+
+struct JordanBlock{T} <: AbstractMatrix{T}
+    λ::T
+    size::Integer
+end
+Base.size(J::JordanBlock) = (J.size, J.size)
+Base.size(J::JordanBlock, d::Integer) = (J.size, J.size)
+function Base.getindex(J::JordanBlock, i::Integer, j::Integer)
+    if i == j
+        return J.λ
+    elseif i == j + 1
+        return one(T)
+    else
+        return zero(T)
+    end
+end
+
+struct JordanCanonicalForm{T} <: AbstractMatrix{T}
+    jordan_blocks::Vector{JordanBlock{T}}
+end
+function Base.size(J::JordanCanonicalForm, d::Integer)
+    n = sum(b -> size(b, 1), J.jordan_blocks)
+    return n
+end
+function Base.size(J::JordanCanonicalForm)
+    n = size(J, 1)
+    return (n, n)
+end
+function Base.getindex(J::JordanCanonicalForm, i::Integer, j::Integer)
+    # FIXME: Compute correct indexing.
+    block = J.jordan_blocks[findfirst(b -> i <= size(b, 1), J.jordan_blocks)]
+    return block[i, j]
+end
+
+mutable struct JordanFactorization{T}
+    basis::AbstractMatrix{T}
+    form::JordanCanonicalForm{T}
+end
+
+Base.iterate(J::JordanFactorization) = (J.basis, Val(:form))
+Base.iterate(J::JordanFactorization, ::Val{:form}) = (J.form, Val(:done))
+Base.iterate(J::JordanFactorization, ::Val{:done}) = nothing
+
+jordan_form(M; kwargs...) = trytoint.(_jordan_form(M; kwargs...))
+function _jordan_form(M::AbstractMatrix{T}) where {T <: IntOrRational} 
+    LinearAlgebra.checksquare(M)
+    
+    eigs = radical_eigvals(M)
+    eigs = algebraic_multiplicity(eigs)
+            
+    # f((eig, size)) = jordan_block(eig, size)
+    # blocks = map(f, block_structure)
+    # jordan_mat = BlockDiagonal(blocks)
+
+    
+    jordan_basis = []
+    jordan_blocks = []
+    
+    for (λ, alg_mul) in eigs
+        λ_basis = []
+
+        println(λ, " ", alg_mul)
+        display(nullspacex(M - λ * I))
+
+        eigvecs = nullspacex(M - λ * I)
+        new_vecs = []
+
+        for v in eachcol(eigvecs)
+
+            vec = eig_mat(λ, i) * [:, 1]
+            push!(λ_basis, vec)
+            push!(jordan_basis, vec)
+        end
+    end
+    
+    basis_mat = hcat(jordan_basis...)
+    
+    return JordanFactorization(basis_mat, JordanCanonicalForm(jordan_blocks))
+end
 
 end
--- src/eigenvalues.jl
+++ src/eigenvalues.jl
@@ -14,6 +14,24 @@ function radical_eigvals(A::AbstractMatrix{T}) where {T <: IntOrRational}
 
         return eigs
     end
+
+    sort!(eigs, by=λ -> (real(λ), imag(λ)))
+
+    return eigs
+end
+
+function algebraic_multiplicity(eigs::AbstractVector{T}) where T
+    mult_eigs = Tuple{T, Int64}[]
+
+    @inbounds for λ in eigs
+        if isempty(mult_eigs) || mult_eigs[end][1] != λ
+            push!(mult_eigs, (λ, 1))
+        else
+            mult_eigs[end] = (λ, mult_eigs[end][2] + 1)
+        end
+    end
+    
+    return mult_eigs
 end
 
 @inline charpoly(A::AbstractMatrix) = berkowitz_vector(A) # Ordered from highest to lowest power
@@ -23,7 +41,7 @@ function berkowitz_vector(A::AbstractMatrix{T}) where {T}
 
     M = T[1, -A[end, end]]
 
-    for i in 2:n
+    @inbounds for i in 2:n
         # Loop from the back as that enables storing a single vector T,
         # rather than storing a matrix for each iteration before reducing.
         
@@ -52,71 +70,209 @@ function berkowitz_vector(A::AbstractMatrix{T}) where {T}
     return M
 end
 
-function charpoly_roots(p::AbstractVector{T}) where {T <: IntOrRational}
+"""
+Try to find symbolic roots of a characteristic polynomial with best effort. 
+By Abel-Ruffini theorem, there is no general solution for polynomials of degree 5 or higher.
+"""
+charpoly_roots(p::AbstractVector) = trytoint.(_charpoly_roots(p))
+function _charpoly_roots(p::AbstractVector{T}) where {T <: IntOrRational}
     @assert length(p) >= 2 && isone(p[1])
 
     # p is ordered from highest to lowest power, hence if the last n coefficients are zero,
-    # then we know that there will be n roots at 0, and we can divide through by x^n.
-    n = findfirst(i -> !iszero(p[i]), n:1) - 1
+    # then we can divide through by x^n and we know that there will be n roots at 0.
+    n = findfirst(i -> !iszero(p[i]), reverse(eachindex(p))) - 1
     eigs = zeros(T, n)
 
     p = p[1:end - n]
-    l = length(p)
     
-    if l == 2  # It is a linear polynomial
-        push!(eigs, -p[2])
-        return eigs
-    elseif l == 3  # It is a quadratic polynomial
-        a, b, c = p
-
-        if iszero(b)
-            r = Symbolics.wrap(Symbolics.Term(sqrt, -c // a))
-            push!(eigs, r)
-            push!(eigs, -r)
-            return eigs
-        end
-
-        d = b^2 - 4a*c
-        if iszero(d)
-            push!(eigs, -b // (2a))
-            push!(eigs, -b // (2a))
-        else
-            push!(eigs, (-b + sqrt(d)) / (2a))
-            push!(eigs, (-b - sqrt(d)) / (2a))
-        end
-
-        return eigs
+    if islinearorquadratic(p)
+        return vcat(eigs, linearorquadraticroots(p))
     end
 
     # Only the leading and constant coefficients are non-zero. Take the nth root.
-    if iszero(p[2:end - 1])
-        a, b = p[1], p[end]
-        d = l - 1
-
-        base = -b
-        α = base^(1 // d)
-
-        roots = [α * exp(k * 2π * im / d) for k in 0:(n - 1)]
-        return vcat(eigs, roots)
+    if isbinomial(p)
+        return vcat(eigs, binomialroots(p))
     end
 
-    if l == 4
-        # TODO: Handle the quartic polynomial case
+    # Check if any of -5:5 are roots
+    @inbounds for r in -5:5
+        r = convert(T, r)
+        while iszero(evalpoly(p, r))
+            push!(eigs, r)
+            p = divfactor(p, r)  # Divide out the factor (x - r)
+
+            if islinearorquadratic(p)
+                return vcat(eigs, linearorquadraticroots(p))
+            end
+        end
     end
 
-    # TODO: Better, more meaningful error message
-    throw(NotImplementedError("The charpoly_roots function has not been implemented for polynomials of degree $l"))
+    if iscubic(p)
+        return vcat(eigs, cubicroots(p))
+    end
+
+    if isquartic(p)
+        return vcat(eigs, quarticroots(p))
+    end
+
+    throw("Unable to find symbolic roots of polynomial")
 end
 
-function symbolic_radical(base, n)
+islinear(p) = length(p) == 2
+function linearroots(p)
+    @assert islinear(p)
+
+    a, b = p
+    @assert isone(a)
+
+    return [-b]
+end
+
+isquadratic(p) = length(p) == 3
+function quadraticroots(p)
+    @assert isquadratic(p)
+
+    a, b, c = p
+    @assert isone(a)
+
+    if iszero(b)
+        r = symbolic_sqrt(-c)
+        return [r, -r]
+    end
+
+    d = b^2 - 4c
+    if iszero(d)
+        r = -b // 2
+        return [r, r]
+    else
+        r1 = (-b + symbolic_sqrt(d)) // 2
+        r2 = (-b - symbolic_sqrt(d)) // 2
+
+        return [r1, r2]
+    end
+end
+
+islinearorquadratic(p) = islinear(p) || isquadratic(p)
+function linearorquadraticroots(p)
+    if islinear(p)
+        return linearroots(p)
+    else
+        return quadraticroots(p)
+    end
+end
+
+isbinomial(p) = iszero(p[2:end - 1])
+function binomialroots(p)
+    @assert isbinomial(p)
+
+    # TODO: Implement
+    a, b = p[1], p[end]
+    @assert isone(a)
+
+    d = l - 1
+
+    base = -b
+    α = base^(1 // d)
+
+    roots = [α * exp(k * 2π * im / d) for k in 0:(n - 1)]
+    return roots
+end
+
+function evalpoly(p, r)
+    coeff = reverse(p)
+    f((i, c)) = c * r^(i - 1)
+    return sum(f, enumerate(coeff))
+end
+
+function divfactor(p::AbstractVector{T}, r::T) where {T}
+    n = length(p) - 1
+    @assert n >= 1
+
+    q_coeff = zeros(T, n)
+    r_coeff = copy(p)
+
+    @inbounds for i in 1:n
+        q = r_coeff[i]
+        q_coeff[i] = q
+
+        r_coeff[i] -= q
+        r_coeff[i + 1] -= -r * q
+    end
+
+    @assert iszero(r_coeff[end]) "r is not a factor of p"
+    return q_coeff
+end
+
+iscubic(p) = length(p) == 4
+function cubicroots(p)
+    @assert iscubic(p)
+
+    # Cardano's method
+    a, b, c, d = p
+    @assert isone(a)
+
+    Q = (3c - b^2) // 9
+    R = (9b * c - 27d - 2b^3) // 54
+
+    D = Q^3 + R^2
+    S = symbolic_cbrt(R + symbolic_sqrt(D))
+    T = symbolic_cbrt(R - symbolic_sqrt(D))
+
+    r1 = S + T - b // 3
+    r2 = -(S + T) // 2 - b // 3 + (S - T) * symbolic_sqrt(3) * 1im // 2 
+    r3 = -(S + T) // 2 - b // 3 - (S - T) * symbolic_sqrt(3) * 1im // 2
+
+    return [r1, r2, r3]
+end
+
+isquartic(p) = length(p) == 5
+function quarticroots(p)
+    @assert isquartic(p)
+
+    a, b, c, d, e = p
+    @assert isone(a)
+
+    # Biquadratic equations
+    if iszero(b) && iszero(d)
+        r1, r2 = quadraticroots([1, c, e])
+        return [symbolic_sqrt(r1), -symbolic_sqrt(r1), symbolic_sqrt(r2), -symbolic_sqrt(r2)]
+    end
+
+    # Ferrari's method
+    q = [1, -c, b * d - 4e, 4c * e - b^2 * e - d^2]
+    croots = cubicroots(q)
+    y = findfirst(isreal, croots)
+
+    p = [
+        b + symbolic_sqrt(b^2 - 4c + 4y),
+        b - symbolic_sqrt(b^2 - 4c + 4y)
+    ]
+
+    q = [
+        y - symbolic_sqrt(y^2 - 4e),
+        y + symbolic_sqrt(y^2 - 4e)
+    ]
+
+    r1 = (-p[1] + symbolic_sqrt(p[1]^2 - 8q[1])) // 4
+    r2 = (-p[1] - symbolic_sqrt(p[1]^2 - 8q[1])) // 4
+    r3 = (-p[2] + symbolic_sqrt(p[2]^2 - 8q[2])) // 4
+    r4 = (-p[2] - symbolic_sqrt(p[2]^2 - 8q[2])) // 4
+
+    return [r1, r2, r3, r4]
+end
+
+function symbolic_sqrt(r)
     if base == 0
         return 0
     end
 
-    if base < 0
-        imagimary = 1im
-        base = -base
+    if r < 0
+        return Symbolic.Pow(-r, 1//2) * 1im
     else
-        return base^(1 / n)
+        return Symbolics.Pow(r, 1//2)
     end
 end
+symbolic_cbrt(r) = Symbolics.Pow(r, 1//3)
+
+trytoint(x::Rational) = isinteger(x) ? convert(Int, x) : x
+trytoint(x) = x
