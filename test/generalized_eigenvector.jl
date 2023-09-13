e4a0e1046c44424cc96510db85c7f13480ddd31d
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-09-13 17:26:36 +0200

    Test for subspace containment instead of exact generalized eigenvector

--- test/generalized_eigenvector.jl
+++ test/generalized_eigenvector.jl
@@ -1,3 +1,10 @@
+using LinearAlgebra
+
+function insubspace(v, subspace)
+    subspace = reduce(hcat, subspace)
+    return rank(hcat(subspace, v)) == rank(subspace)
+end
+
 # 1x1 matrix - trivial
 alg_mult = [(1, 1)]
 λ_basis, blocks = JordanForm.generalized_eigenvectors(O1 // 1, 1, 1)
@@ -6,7 +13,7 @@ expected_block_sizes = [(1, 1)]
 @test length(blocks) == 1
 @test expected_block_sizes == map(size, blocks)
 λ_basis = map(v -> JordanForm.unwrap.(v), λ_basis)
-@test λ_basis ≈ [[1.0]] # FIXME: Test for subspace containment rather than exact eigenvectors
+@test insubspace(λ_basis[1], [[1.0]])
 
 # 2x2 matrices
 alg_mult = [(7 - JordanForm.symbolic_sqrt(41) * 1im, 1), (7 + JordanForm.symbolic_sqrt(41) * 1im, 1)]
@@ -16,8 +23,7 @@ expected_block_sizes = [(1, 1)]
 @test length(blocks) == 1
 @test expected_block_sizes == map(size, blocks)
 λ_basis = map(v -> JordanForm.unwrap.(v), λ_basis)
-println(λ_basis[1])
-@test λ_basis ≈ [[1 / 6 + 41^(1 / 2) * 1im / 6, 1]] # FIXME: Test for subspace containment rather than exact eigenvectors
+@test insubspace(λ_basis[1], [[1 / 6 + 41^(1 / 2) * 1im / 6, 1]])
 
 λ_basis, blocks = JordanForm.generalized_eigenvectors(A1 // 1, alg_mult[2][1], alg_mult[2][2])
 expected_block_sizes = [(1, 1)]
@@ -25,4 +31,4 @@ expected_block_sizes = [(1, 1)]
 @test length(blocks) == 1
 @test expected_block_sizes == map(size, blocks)
 λ_basis = map(v -> JordanForm.unwrap.(v), λ_basis)
-@test λ_basis ≈ [[1 / 6 - 41^(1 / 2) * 1im / 6, 1]] # FIXME: Test for subspace containment rather than exact eigenvectors
+@test insubspace(λ_basis[1], [[1 / 6 - 41^(1 / 2) * 1im / 6, 1]])
