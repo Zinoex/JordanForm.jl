6bdaad2840bccd7a0176caee37b265392e87e834
Frederik Baymler Mathiesen <frederik@baymler.com>
2023-09-13 17:21:43 +0200

    Switch to approximate generalized eigenvector test

--- test/generalized_eigenvector.jl
+++ test/generalized_eigenvector.jl
@@ -6,7 +6,7 @@ expected_block_sizes = [(1, 1)]
 @test length(blocks) == 1
 @test expected_block_sizes == map(size, blocks)
 λ_basis = map(v -> JordanForm.unwrap.(v), λ_basis)
-@test λ_basis == [[1]] # FIXME: Test for subspace containment rather than exact eigenvectors
+@test λ_basis ≈ [[1.0]] # FIXME: Test for subspace containment rather than exact eigenvectors
 
 # 2x2 matrices
 alg_mult = [(7 - JordanForm.symbolic_sqrt(41) * 1im, 1), (7 + JordanForm.symbolic_sqrt(41) * 1im, 1)]
@@ -17,7 +17,7 @@ expected_block_sizes = [(1, 1)]
 @test expected_block_sizes == map(size, blocks)
 λ_basis = map(v -> JordanForm.unwrap.(v), λ_basis)
 println(λ_basis[1])
-@test λ_basis == [[1 // 6 + 41^(1 // 2) * 1im // 6, 1]] # FIXME: Test for subspace containment rather than exact eigenvectors
+@test λ_basis ≈ [[1 / 6 + 41^(1 / 2) * 1im / 6, 1]] # FIXME: Test for subspace containment rather than exact eigenvectors
 
 λ_basis, blocks = JordanForm.generalized_eigenvectors(A1 // 1, alg_mult[2][1], alg_mult[2][2])
 expected_block_sizes = [(1, 1)]
@@ -25,4 +25,4 @@ expected_block_sizes = [(1, 1)]
 @test length(blocks) == 1
 @test expected_block_sizes == map(size, blocks)
 λ_basis = map(v -> JordanForm.unwrap.(v), λ_basis)
-@test λ_basis == [[1 // 6 - 41^(1 // 2) * 1im // 6, 1]] # FIXME: Test for subspace containment rather than exact eigenvectors
+@test λ_basis ≈ [[1 / 6 - 41^(1 / 2) * 1im / 6, 1]] # FIXME: Test for subspace containment rather than exact eigenvectors
