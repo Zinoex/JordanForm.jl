17bfbacc711a112322567c74ce0fcbcb461f040e
Frederik Mathiesen <frederik_mathies@hotmail.com>
2023-09-27 21:15:53 +0200

    Test the complete Jordan form

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,22 @@
-using JordanForm, SymbolicUtils
+using JordanForm, SymbolicUtils, LinearAlgebra
 using Test
 
-test_files = ["charpoly.jl", "eigenvalue.jl", "generalized_eigenvector.jl", "jordan_form.jl"]
+function insubspace(v, subspace)
+    subspace = reduce(hcat, subspace)
+    return rank(hcat(subspace, v)) == rank(subspace)
+end
+
+function islinearlyindependent(vectors::AbstractVector{<:AbstractVector})
+    return rank(reduce(hcat, vectors)) == length(vectors)
+end
+
+function islinearlyindependent(vectors::AbstractMatrix)
+    return rank(vectors) == size(vectors, 2)
+end
+
 include("matrix_data.jl")
     
+test_files = ["charpoly.jl", "eigenvalue.jl", "generalized_eigenvector.jl", "jordan_form.jl"]
 for f = test_files
     @testset "$f" begin
         include(f)
